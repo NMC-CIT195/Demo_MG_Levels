8209845a4f068ecbf0b20d6def38d684dfd1dc26
John Velis <john@johnvelis.com>
2016-04-16 09:44:48 -0400

    initial commit

---
+++ Demo_MG_Levels/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo_MG_Levels
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MazeGame())
+                game.Run();
+        }
+    }
+#endif
+}
